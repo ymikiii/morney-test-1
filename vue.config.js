2bc703d213650ef64c2d31e742dbcf4e98b455f9
ymikiii <1271311538@qq.com>
2021-10-14 18:51:36 +0800

    添加全局样式和全局变量

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
             .include.add(dir).end() // 包含 icons 目录
             .use('svg-sprite-loader-mod').loader('svg-sprite-loader-mod').options({extract: false}).end()
             .use('svgo-loader').loader('svgo-loader')
-            .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
+            //.tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
         // eslint-disable-next-line @typescript-eslint/no-var-requires
         config.plugin('svg-sprite').use(require('svg-sprite-loader-mod/plugin'), [{plainSprite: true}])
         config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons 目录
