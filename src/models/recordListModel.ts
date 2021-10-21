743f81fcaa306336c433fa3c991b3e013cad63bb
ymikiii <1271311538@qq.com>
2021-10-21 11:22:32 +0800

    更新â

---
+++ src/models/recordListModel.ts
@@ -0,0 +1,21 @@
+import clone from '@/lib/clone';
+
+const localStorageKeyName = 'recordList';
+const recordListModel = {
+    data: [] as RecordItem[],
+    create(record:RecordItem) {
+        const record2: RecordItem = clone(record);
+        record2.createdAt = new Date();
+        this.data.push(record2);
+    },
+
+    fetch() {
+        this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+        return this.data;
+    },
+    save() {
+        window.localStorage.setItem(localStorageKeyName,
+            JSON.stringify(this.data));
+    }
+};
+export default recordListModel;
